fb6cafeee45631330700c87fd4f1d4cb1150fecd
Gabriel <gabloon1@tuta.io>
2021-11-12 16:52:48 -0300

    Update FileDownloader.java
    Android 10+ support

--- android/src/main/java/com/veluxa/plugins/FileDownloader.java
+++ android/src/main/java/com/veluxa/plugins/FileDownloader.java
@@ -1,129 +1,158 @@
 package com.veluxa.plugins;
 
+import android.Manifest;
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 
-import android.Manifest;
-import android.app.DownloadManager;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Environment;
-
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import android.util.Log;
 
 @NativePlugin
 public class FileDownloader extends Plugin{
 
-    private final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 10001;
-    //下载器
-    private DownloadManager downloadManager;
-    private Context mContext;
-    //下载的ID
-    private long downloadId;
-    private String pathstr;
+  private final int PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 10001;
+  //下载器
+  private DownloadManager downloadManager;
+  private Context mContext;
+  //下载的ID
+  private long downloadId;
+  private String pathstr;
 
-    PluginCall _call;
+  PluginCall _call;
 
-    @PluginMethod
-    public void download(PluginCall call) {
-        _call = call;
-        mContext = getContext();
-        requestPermissions();
+  private final String outputDir = "Click";
 
-        call.save();
-        downloadFile(call);
+  @PluginMethod
+  public void download(PluginCall call) {
+    _call = call;
+    mContext = getContext();
+    requestPermissions();
+
+    call.save();
+    downloadFile(call);
+  }
+
+  //获取权限
+  private void requestPermissions() {
+
+    if (!this.hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) && !this.hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+      this.pluginRequestPermissions(
+        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},
+        PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
     }
+  }
 
-    //获取权限
-    private void requestPermissions() {
+  //下载文件
+  private void downloadFile(final PluginCall call) {
+    String url = call.getString("url","");
+    String filename = call.getString("filename","");
+    OutputStream outputStream;
 
-        if (!this.hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) && !this.hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            this.pluginRequestPermissions(
-                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},
-                    PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
-        }
-    }
+    HttpURLConnection connection = null;
 
-    //下载文件
-    private void downloadFile(final PluginCall call) {
-        String url = call.getString("url","");
-        String filename = call.getString("filename","");
+    try {
+      connection = (HttpURLConnection) new URL(url).openConnection();
+      connection.setRequestMethod("GET");
+      connection.setConnectTimeout(8000);
+      connection.setReadTimeout(8000);
 
-        //创建下载任务
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
-        //移动网络情况下是否允许漫游
-        request.setAllowedOverRoaming(false);
-        //在通知栏中显示，默认就是显示的
-        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
-        request.setTitle("文件下载器");
-        request.setDescription(filename + "下载中...");
-        request.setVisibleInDownloadsUi(true);
+      InputStream inputStream = connection.getInputStream();
+      byte[] buffer = new byte[1024];
 
-        //设置下载的路径
-        File file = new File(mContext.getExternalFilesDir(""), filename);
-        request.setDestinationUri(Uri.fromFile(file));
-        pathstr = file.getAbsolutePath();
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) { // To Download File for Android 10 and above
+        BufferedInputStream bis = new BufferedInputStream(inputStream);
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.MediaColumns.DISPLAY_NAME, filename);
 
-        //获取DownloadManager
-        if (downloadManager == null)
-            downloadManager = (DownloadManager) mContext.getSystemService(Context.DOWNLOAD_SERVICE);
-        //将下载请求加入下载队列，加入下载队列后会给该任务返回一个long型的id，通过该id可以取消任务，重启任务、获取下载的文件等等
-        if (downloadManager != null) {
-            downloadId = downloadManager.enqueue(request);
-        }
+        String desDirectory = Environment.DIRECTORY_DOWNLOADS;
+        // If you want to create custom directory inside Download directory only
+        desDirectory = desDirectory + File.separator + outputDir;
+        File desFile = new File(desDirectory);
+        if (!desFile.exists()) desFile.mkdir();
+        // final output path
+        pathstr = desDirectory + File.separator + filename; // pathstr is outputPath
 
-        //注册广播接收者，监听下载状态
-        mContext.registerReceiver(receiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
-    }
-
-    //广播监听下载的各个状态
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            checkStatus();
-        }
-    };
-
-    //检查下载状态
-    private void checkStatus() {
-        DownloadManager.Query query = new DownloadManager.Query();
-        //通过下载的id查找
-        query.setFilterById(downloadId);
-        Cursor cursor = downloadManager.query(query);
-        if (cursor.moveToFirst()) {
-            int status = cursor.getInt(cursor.getColumnIndex(DownloadManager.COLUMN_STATUS));
-            switch (status) {
-                //下载暂停
-                case DownloadManager.STATUS_PAUSED:
-                    break;
-                //下载延迟
-                case DownloadManager.STATUS_PENDING:
-                    break;
-                //正在下载
-                case DownloadManager.STATUS_RUNNING:
-                    break;
-                //下载完成
-                case DownloadManager.STATUS_SUCCESSFUL:
-                    //下载完成
-                    cursor.close();
-                    JSObject ret = new JSObject();
-                    ret.put("path", pathstr);
-                    _call.success(ret);
-                    break;
-                //下载失败
-                case DownloadManager.STATUS_FAILED:
-                    cursor.close();
-                    mContext.unregisterReceiver(receiver);
-                    _call.reject("下载失败");
-                    break;
+        values.put(MediaStore.MediaColumns.RELATIVE_PATH, desDirectory);
+        Uri uri = mContext.getContentResolver().insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, values);
+        if (uri != null) {
+          outputStream = mContext.getContentResolver().openOutputStream(uri);
+          if (outputStream != null) {
+            BufferedOutputStream bos = new BufferedOutputStream(outputStream);
+            int bytes;
+            while ((bytes = bis.read(buffer)) != -1) {
+              bos.write(buffer, 0, bytes);
+              bos.flush();
             }
+            bos.close();
+          }
         }
+        bis.close();
+
+      } else {
+
+        // first we create app name folder direct to the root directory
+        String destURL = Environment.getExternalStorageDirectory().getPath() + File.separator + outputDir;
+        File desFile = new File(destURL);
+        if (!desFile.exists()) {
+          desFile.mkdir();
+        }
+
+        // once the app name directory created we create download directory inside app directory
+        destURL = destURL + File.separator + Environment.DIRECTORY_DOWNLOADS;
+        desFile = new File(destURL);
+        if (!desFile.exists()) {
+          desFile.mkdir();
+        }
+
+        destURL = destURL + File.separator + filename;
+
+        // final output path
+        pathstr = destURL;
+
+        outputStream = new FileOutputStream(destURL);
+        BufferedOutputStream bos = new BufferedOutputStream(outputStream);
+        int bytes;
+        while ((bytes = inputStream.read(buffer)) != -1) {
+          outputStream.write(buffer, 0, bytes);
+        }
+
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+      }
+
+      JSObject ret = new JSObject();
+      ret.put("path", pathstr);
+      _call.success(ret);
+    } catch (Exception e) {
+      _call.reject(e.getMessage());
     }
+
+  }
 }
