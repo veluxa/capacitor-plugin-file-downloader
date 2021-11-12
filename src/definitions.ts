1c2e464578387a51dfc89f44c0604a090af47e33
Gabriel <gabloon1@tuta.io>
2021-11-12 16:52:12 -0300

    Update definitions.ts
    Capacitor 3+ support

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,9 +1,3 @@
-declare module '@capacitor/core' {
-  interface PluginRegistry {
-    FileDownloader: FileDownloaderPlugin;
-  }
-}
-
 export interface FileDownloaderPlugin {
   download(options: { url: string, filename: string }): Promise<any>;
 }
